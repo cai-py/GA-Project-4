b8d3970ce41b3b33df9bc4bd0060aa93fbd96c75
Andre Le <dre@Andres-MBP-35.home>
2020-10-20 02:00:49 -0400

    changed css

--- components/how.js
+++ components/how.js
@@ -2,24 +2,24 @@ class How extends React.Component {
   render = () => {
     return (
       <div>
-      <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
+      <button type="button" className="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
         How it works
       </button>
 
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">How it works</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">How it works</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <h6>You can view the posts that users have made. If you would like to add a post you can add a post by using the button "Add Post". Enjoy!</h6>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
